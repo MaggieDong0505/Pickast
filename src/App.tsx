8eddf5086e30255b81121425ed8761f0ea5fda1e
Maggie <maggiedong76@gmail.com>
2026-06-13 18:31:10 +0800

    修改2

--- src/App.tsx
+++ src/App.tsx
@@ -868,7 +868,7 @@ export default function App() {
 
         <div className="relative flex flex-1 flex-col overflow-hidden pb-[calc(96px+env(safe-area-inset-bottom,0px))]">
           {activeTab === 'curated' ? (
-            <div className="flex h-full flex-1 flex-col justify-center overflow-hidden px-4 pb-2 pt-0 md:px-5">
+            <div className="flex h-full flex-1 flex-col justify-start overflow-hidden px-4 pb-2 pt-0 md:px-5">
               <div className="mt-3 flex items-center justify-between border-b border-black/10 pb-4 select-none">
                 <div className="flex items-baseline gap-1">
                   <span className="font-serif font-black text-xl tracking-tight text-[#1A1A1A]">听荐</span>
